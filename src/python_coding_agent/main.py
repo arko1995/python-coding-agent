d5ba8dfbe9858c6383a1581bb218c49a72fe5b29
arko1995 <shahriarsohelarko@gmail.com>
2026-09-02 14:18:56 +0600

    added basic prompt-response and a verbose flag

--- src/python_coding_agent/main.py
+++ src/python_coding_agent/main.py
@@ -1,15 +1,41 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
 
 load_dotenv()
+
 
 def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    print(api_key)
+    if len(sys.argv) < 2:
+        print("Please provide a prompt")
+        sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+    verbose_flag = False
+
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+
+    prompt = sys.argv[1]
+
+    message = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-3.6-flash",
+        contents=f"{message}",
+    )
+
+    print("Response: ", response.text)
+
+    if response is None or response.usage_metadata is None:
+        return
+
+    if verbose_flag:
+        print("User Prompt: ", prompt)
+        print("Response Token: ", response.usage_metadata.prompt_token_count)
+        print("Candidate Token: ", response.usage_metadata.candidates_token_count)
